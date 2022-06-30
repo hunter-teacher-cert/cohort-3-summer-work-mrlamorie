b80a6ab9bb87ea6111ab67c673297e717ec3f825
William LaMorie <47149910+mrlamorie@users.noreply.github.com>
2022-06-30 14:33:03 +0000

    Initial push of Nim

---
+++ programming/1/Nim.java
@@ -0,0 +1,66 @@
+/**
+ * Game of Nim by Team BossCoders
+ * William LaMorie
+ * collaborators: First Last, First Last
+ */
+import java.io.*;
+import java.util.*;
+import java.lang.Math.*;
+
+/*
+bag contains 12 stones initially
+a move consists of removing 1-3 stones from the bag
+prompt user for number of stones they wish to remove each turn
+tell user how many stones the AI removed and how many stones remain
+facilitate play until human or AI wins, and announce winner
+*/
+
+public class Nim {
+
+  
+  public static void main(String[] args){
+
+    int stones = 12; // bag contains 12 stones initially    
+    int stonesTaken; // check for valid range?
+    
+    Scanner input = new Scanner(System.in); // scanner should run fine here
+    Random num = new Random();
+    
+    // facilitate play until human or AI wins, and announce winner
+    while(stones > 0){
+      System.out.println("There are currently " + stones + " stones left.");
+      
+      // player move
+      
+      // prompt user for number of stones they wish to remove each turn
+      System.out.print("How many stones (1-3) would you like to take? ");
+      stonesTaken = input.nextInt();
+      while(stonesTaken > 3){
+        System.out.print("Please make a choice from 1 - 3 ");
+        stonesTaken = input.nextInt();
+      }
+      stones -= stonesTaken;
+
+      // check for a player win
+      if (stones <= 0) {
+        System.out.println("Player Wins! ");
+        break;
+      }
+
+      // AI move
+      stonesTaken = num.nextInt(3) + 1;
+      if (stonesTaken > stones){ stonesTaken = stones; }
+      // a move consists of removing 1-3 stones from the bag
+      System.out.println("The computer takes " + stonesTaken + " stones.");
+      stones -= stonesTaken;
+      // check for win
+      if (stones <= 0) {
+        System.out.println("Computer Wins! ");
+        break;
+      }      
+      
+    } 
+    
+  }
+  
+} 
