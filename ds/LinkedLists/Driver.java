b9c7e88aa0e2c5afe3e090878b47276ab738fb40
William LaMorie <47149910+mrlamorie@users.noreply.github.com>
2022-07-13 12:44:04 +0000

    tested error returns for getter

--- ds/LinkedLists/Driver.java
+++ ds/LinkedLists/Driver.java
@@ -5,6 +5,7 @@ public class Driver{
   public static void main(String[] args) {
     LinkedList l = new LinkedList();
     System.out.println(l.size()); // 0 item test
+    //System.out.println(l.get(0)); // empty LL error check // PASSED
     l.add(0, "C"); // add override test with i == 0
     System.out.println(l.size()); // 1 item test
     l.add("A"); // add 1 param test
@@ -15,7 +16,10 @@ public class Driver{
     System.out.println(l.size()); // 3 item test
     //l.add(-1, "Fail"); // negative index error check //PASSED
     //l.add(34738, "Fail"); //IOOB error check // PASSED
+    //System.out.println(l.get(-1)); // negative index error check //PASSED
+    //System.out.println(l.get(3432535)); //IOOB error check // PASSED
     // get testing & correct use testing for above
     System.out.println(l.get(0) + " " + l.get(1) + " " + l.get(2) + " " + l.get(3));
+    
   }
 }
