b45a59aac5f655e1d6a9cd48fa432bcdeacc84a0
William LaMorie <47149910+mrlamorie@users.noreply.github.com>
2022-07-13 12:05:19 +0000

    tested error throws for out of bounds and neg index

--- ds/LinkedLists/Driver.java
+++ ds/LinkedLists/Driver.java
@@ -7,9 +7,13 @@ public class Driver{
     System.out.println(l.size()); // 0 item test
     l.add(0, "A"); // add override test with i == 0
     System.out.println(l.size()); // 1 item test
-    l.add("B"); // add 1 param test
+    l.add("C"); // add 1 param test
     System.out.println(l.size()); // 2+ item test
-    l.add(1, "C"); // overreiden add insert test
+    l.add(1, "B"); // overreiden add insert test
     System.out.println(l.size()); // 3 item test
+    l.add(3, "D"); // overridden append to end test
+    System.out.println(l.size()); // 3 item test
+    //l.add(-1, "Fail"); // negative index error check //PASSED
+    l.add(34738, "Fail"); //IOOB error check // PASSED
   }
 }
