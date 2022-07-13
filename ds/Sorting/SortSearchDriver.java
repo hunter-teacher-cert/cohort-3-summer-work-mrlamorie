a07ef01a28f49cdb54a5c67d07d2a6fc3389783c
William LaMorie <47149910+mrlamorie@users.noreply.github.com>
2022-07-13 18:57:27 +0000

    tested findSmallesIndex

--- ds/Sorting/SortSearchDriver.java
+++ ds/Sorting/SortSearchDriver.java
@@ -11,16 +11,16 @@ public class SortSearchDriver {
   	System.out.println(ss);
   	
   		
-  	// Uncomment these to test part 2
+  	//Uncomment these to test part 2
   
-  	// int i;
-  	// i = ss.findSmallestIndex(0);
-  	// System.out.println("ss["+ i +"] = "+ss.get(i)+" : " + ss);
+  	int i;
+  	i = ss.findSmallestIndex(0);
+  	System.out.println("ss["+ i +"] = "+ss.get(i)+" : " + ss);
   
-  	// i = sd2.findSmallestIndex(3);
-  	// System.out.println("ss["+ i + "] = "+ss.get(i)+" : " + ss);
+  	i = ss.findSmallestIndex(3);
+  	System.out.println("ss["+ i + "] = "+ss.get(i)+" : " + ss);
   	
-  	// Uncomment these to test part 3
+  	//Uncomment these to test part 3
   
   	// System.out.println(ss);
   	// ss.sort();
