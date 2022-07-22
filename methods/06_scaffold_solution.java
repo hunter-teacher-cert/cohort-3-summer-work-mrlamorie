b0593ae82fc9e6a89d3bf70f072f5ae5719a3624
William LaMorie <47149910+mrlamorie@users.noreply.github.com>
2022-07-22 00:03:57 +0000

    added 06 files

---
+++ methods/06_scaffold_solution.java
@@ -0,0 +1,44 @@
+public class Loops{
+
+    public static void main(String[] args) {
+
+      // make a loop that runs 10 times
+      for(int i =0; i < 10; i ++){
+
+        // add a message to let people know the outer loop is running
+        System.out.println("I am the outer loop");
+
+        // make a loop INSIDE the loop you made, run it 5 times
+        // if this loop seems to happen too few times, or if you get an error, talk it out with
+        // your group, and then check with me to see if you've figured out why
+        for(int j = 0; j < 5; j++){  // students will often use i again, causing errors
+          // add a message to let people know the inner loop is running
+          System.out.println("I am the inner loop");
+        }
+      }
+
+
+      // expand and intigrate activites
+      // make a loop that runs 10 times
+      for(int i =0; i < 10; i ++){
+        // add a message to let people know the outer loop is running
+        System.out.println("I am the outer loop");
+        // if i is even
+          // then loop 10 times
+            // add a message to let people know the inner loop is running
+            // every other loop print "i and j are both even!"
+        if((i % 1) == 0){
+          for(int j = 0; j < 10; j++){  // students will often use i again, causing errors
+            // add a message to let people know the inner loop is running
+            System.out.println("I am the inner loop");
+            if((j % 1) == 0){
+              System.out.println("i and j are both even!");
+            }
+          }
+        }
+
+      }
+	
+    }
+
+}
