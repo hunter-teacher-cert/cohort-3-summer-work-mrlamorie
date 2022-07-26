7f1d29fc11f3dd3c806052c5add21b587120e7ac
William LaMorie <47149910+mrlamorie@users.noreply.github.com>
2022-07-26 16:37:38 +0000

    moved some comments around

--- ds/StackQueues/Queue.java
+++ ds/StackQueues/Queue.java
@@ -1,3 +1,17 @@
+/** Queue
+
+Recall, a queue is a First In First Out structure.
+
+You should implement the following methods
+
+- A constructor with no parameters
+- void enqueue(int value) - add value to the end of the queue
+- int dequeue() - remove and return the top value from front of the queue
+- int front() - return but do not remove the top value from the front of the queue
+- boolean isEmpty() - return true of the queue is empty, false otherwise
+- int size() - return the number of elements currently in the queue
+- boolean isFull() - returns true if the queue is full, false otherwise - only implement this if you use an array for implementation
+  */
 public class Queue{
 
     // Place private instance variables here
--- ds/StackQueues/Stack.java
+++ ds/StackQueues/Stack.java
@@ -1,3 +1,18 @@
+/** Stack
+
+Recall, a stack is a Last In First Out structure. // like head of Linked List
+
+You should implement the following methods
+
+- A constructor with no parameters
+- void push(int value) - add value to the top of the stack
+- int pop() - remove and return the top value from the stack
+- int top() - return but do not remove the top value from the stack
+- boolean isEmpty() - return true of the stack is empty, false otherwise
+- int size() - return the number of elements currently in the stack
+- boolean isFull() - returns true if the stack is full, false otherwise - only implement this if you use an array for implementation
+*/
+
 public class Stack{
 
     // Place private instance variables here
